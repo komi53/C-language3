69472c5f9d3f554aceaec76c3503fba94db66fb4
komi53 <kxs53kuro@gmail.com>
2020-10-13 23:44:40 +0900

    mini fix

--- mini.c
+++ mini.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(void){
     int n_1, n_2, n_3, n_4, n_5;
-    int i, j, min;
+    int i, min;
     scanf("%i %i %i %i %i", &n_1, &n_2, &n_3, &n_4, &n_5);
-    int number[4] = {n_1, n_2, n_3, n_4, n_5};
+    int number[5] = {n_1, n_2, n_3, n_4, n_5};
     min = number[0];
     for(i=1; i<5; i++){
         if(min > number[i]){
