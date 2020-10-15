6e220cade535861702efa85f2d839987bcb43960
komi53 <kxs53kuro@gmail.com>
2020-10-16 00:13:17 +0900

    unagiya jugd  add

--- kansu.c
+++ kansu.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int cal(int r) {
-    int area = 2 * r;
+int cal(int r[]) {
+    int area = 2 * r[0];
     return area;
 }
 
 int main(void) {
-  int m, n;
-  scanf("%i", &n);
+  int m, n[100];
+  scanf("%i %i", &n[0], &n[1]);
   if( cal(n) == 4){
     printf("%i", cal(n));
   }
--- unagiya.c
+++ unagiya.c
@@ -1,37 +1,41 @@
 #include <stdio.h>
-int jud(int r) {
-    int area = 2 * r;
-    return area;
+int judg(int chair[], int b_i[], int people, int i) {
+  int ans;
+  for(int j = b_i[i]; j < people; j++){
+    if(chair[j] == 1){
+        ans = 1;
+        break;
+    }else if(chair[j] == 0){
+        ans = 0;
+    }
+  }
+  return ans;
 }
 
 int main(void) {
   int m, n;
   int a_i[1000], b_i[1000];
-  int chair[];
+  int chair[1000];
   int people;
-  int i, j;
+  int i, j, k;
   scanf("%i %i", &m, &n );
-  // 椅子に0を代入椅子の個数を数える
+                                     // 椅子に0を代入椅子の個数を数える
   for(i = 0; i < m; i++){
     chair[i] = 0;
   }
-  // 人数代入
+                                    // 人数代入
   for(i = 0; i < n; i++){
     scanf("%i %i", &a_i[i], &b_i[i]);
     people = (b_i[i] + a_i[i] - 1);
-    while (i < 2){
-      printf("こんにちは¥n");
-      i++;
-    }
-    for(j = b_i[i]; j < people; j++){
+                                    //　判別後、OKならchairに1を代入
+    if(judg(chair, b_i, people, i) == 0){
+    for(j = b_i[i] - 1; j < people; j++){
       chair[j] = 1;
     }
+    }
   }
-  
-
-
-  //for(j = 0; j < n; j++){
-  //  printf("%i %i \n", a_i[j], b_i[j]);
-  //}
+  for(k = 0; k < m; k++){
+    printf("%i \n", chair[k]);
+  }
   return 0;
 }
