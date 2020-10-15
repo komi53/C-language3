e21d8419427c69cadd3ef8fddfa0a6676c785251
komi53 <kxs53kuro@gmail.com>
2020-10-15 18:13:39 +0900

    scanf add

--- unagiya.c
+++ unagiya.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main(void) {
+  int m, n;
+  int a_i[1000], b_i[1000];
+  int i, j;
+  scanf("%i %i", &m, &n );
+  for(i = 0; i < n; i++){
+    scanf("%i %i", &a_i[i], &b_i[i]);
+  }
+  //for(j = 0; j < n; j++){
+  //  printf("%i %i \n", a_i[j], b_i[j]);
+  //}
+  
 
-    return 0;
+  return 0;
 }
